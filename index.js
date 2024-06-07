5f80d5556c54d286bc8c4d9bfb28913fdd4be059
mayurrohokale <mayurrohokale12345@gmail.com>
2024-06-08 00:13:18 +0530

    added JWT token verification API

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv').config({path:'.env'});
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./schema/userSchema");
 const cors = require("cors");
 const PORT = 8000;
 const MONGO_URL =
-  process.env.mongourl ||
-  "mongodb+srv://mayurrohokale12345:EVKCkMOlYPhczrgy@cluster2.nsvpfy1.mongodb.net/";
+  process.env.mongourl || null;
+
+const JWT_SECRET = process.env.jwtsecret || "secret";
 
 const app = express();
 
@@ -23,71 +26,74 @@ mongoose
   });
 
 // User SignUp
-app.post("/signup", async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { name, password, email } = req.body;
   try {
-    if (!name || !password || !email) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email is already taken" });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ email, name, password: hashedPassword });
-    const user_data = { name, email, _id: user._id.toString() };
-
-    res.status(200).json({ message: "User created successfully", user: user_data });
+      if (!name || !password || !email) {
+          return res.status(400).json({ message: 'All fields are required' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({ email, name, password: hashedPassword })
+      const user_data = { name: name, email: email, _id: String(user._id) };
+      const token = jwt.sign(
+        user_data,
+        JWT_SECRET
+      );
+      res.status(200).json({ message: 'Signup successful', token, user :user_data});
   } catch (err) {
-    console.error("Error creating user:", err);
-    res.status(500).json({ message: "Internal Server Error" });
+      res.status(400).json({ message: 'Email already taken' });
   }
 });
 
-// Get all users
-app.get("/users", (req, res) => {
-  User.find()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: "Internal Server Error" });
-    });
+// User login
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+  }
+  const token = jwt.sign(
+    { name: user.name, email: user.email, _id: String(user._id) },
+    JWT_SECRET
+  );
+  res.status(200).json({ message: 'Login successful', token, user : {name: user.name, email: user.email}});
 });
 
-// User Login
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
 
-    User.findOne({ email })
-        .then(user => {
-            if (!user) {
-                return res.status(400).json({ message: "Invalid Credentials" });
-            }
+// Middleware for verifying JWT
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).json({ message: 'Unauthorized' });
+      }
+      req.user = decoded;
+      next();
+  });
+};
 
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (!isMatch) {
-                        return res.status(400).json({ message: "Invalid Credentials" });
-                    }
 
-                    res.status(200).json({ message: "User logged in successfully" });
-                })
-                .catch(err => {
-                    console.error(err);
-                    res.status(500).json({ message: "Server error" });
-                });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ message: "Server error" });
-        });
+app.get('/me', verifyToken, async (req, res) => {
+  
+  try {
+    const check_user = await User.findOne({ email: req.user.email });
+    if (!check_user) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    
+    
+  } else {
+    res.status(200).json({ message: 'User fetched successfully', user: {name: check_user.name, email: check_user.email}});
+  }
+} catch (err) {
+  res.status(400).json({ message: 'Something went wrong' });
+}
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

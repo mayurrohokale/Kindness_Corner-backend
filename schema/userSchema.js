19088e8357aabd522094c3dfac3b29edcd123f7c
mayurrohokale <mayurrohokale12345@gmail.com>
2024-06-13 02:00:56 +0530

    created API for volunteers

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -6,6 +6,9 @@ const userSchema = mongoose.Schema({
   password: String,
   role: { type: String, default: "user" },
   created_at: { type: Date, default: Date.utcnow },
+  phone: {type: String},
+  address: {type:String, unique: false},
+  is_volunteer: {type: Boolean, default:false},
 });
 
 const User = mongoose.model('User', userSchema);

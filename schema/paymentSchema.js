d92c0d4a89f122a25f1de85a1b2da7e535846b77
mayurrohokale <mayurrohokale12345@gmail.com>
2024-06-14 17:04:28 +0530

    updated APIS for Volunteers

---
+++ schema/paymentSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+const paymentSchema = new Schema({
+    razorpay_order_id: {
+        type: String,
+        required: true,
+    },
+    razorpay_payment_id: {
+        type: String,
+        required: true,
+    },
+    razorpay_signature: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Payment =  mongoose.model('Payment', paymentSchema);
+
+module.exports = Payment;

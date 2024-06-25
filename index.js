b9a429bb2435ca5c70d92c452071ab0c6cd04e9f
mayurrohokale <mayurrohokale12345@gmail.com>
2024-06-25 10:52:46 +0530

    added admin-login API

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ mongoose
     console.error("MongoDB Connection Error: ", err);
   });
 
+
+
 // User SignUp
 app.post("/signup", async (req, res) => {
   const { name, password, email } = req.body;
@@ -86,6 +88,63 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
+
+// isAdmin
+const isAdmin = async (req, res, next) => {
+
+  user = await User.findOne({email : req?.user?.email});
+
+  if(user?.role !== 'admin'){
+
+    return res.status(401).json({ message: "Not Access" });
+  }
+  next();
+  
+}
+
+app.post("/testadmin", [verifyToken,isAdmin], async (req, res) => {
+  try{
+    return res.status(200).json({message: "success"});
+  }catch{
+    return res.status(401).json({message: "Not Access"});
+  }
+})
+
+app.post("/admin-login", async (req, res)=> {
+  try{
+    const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+  if(user?.role !== 'admin'){
+    return res.status(401).json({message: "UnAuthorized Access"})
+  }
+  const token = jwt.sign(
+    { name: user.name, email: user.email, _id: String(user._id) },
+    JWT_SECRET
+  );
+  res.status(200).json({
+    message: "Login successful",
+    token,
+    user: { name: user.name, email: user.email },
+  });
+  }catch{
+    return res.status(401).json({message: "Invalid Credentials"});
+  }
+})
+
+//get all users
+
+app.get("/users", [verifyToken, isAdmin], async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
 
 app.get("/me", verifyToken, async (req, res) => {
   try {

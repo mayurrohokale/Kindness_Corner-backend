e13120d27875b51566bf92e98d998f072c977c7b
mayurrohokale <mayurrohokale12345@gmail.com>
2024-07-04 13:57:03 +0530

    Created Add blog API

---
+++ schema/blogSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;

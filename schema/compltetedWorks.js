5f2c83b25f3e297f00e41c2f7d6333825ef30533
mayurrohokale <mayurrohokale12345@gmail.com>
2024-08-21 18:32:14 +0530

    updated schema of Completed works Section

--- schema/compltetedWorks.js
+++ schema/compltetedWorks.js
@@ -11,7 +11,11 @@ const WorksSchema = new mongoose.Schema({
             type: String,
             required: true
             
-        }
+        },
+        image: {
+            type: String,
+            required: true
+        },
     
 });
 
